60069e578645e680ecbf79594c61abe772c115b2
Jonathan Martin <me@nybblr.com>
2013-04-15 18:38:41 -0400

    Added desired tm.

--- machine.py
+++ machine.py
@@ -2,8 +2,8 @@ import sys
 from utility import *
 
 class TuringMachine:
-	def __init__(self, input, states, trans):
-		self.sm = StateMachine(self, states, trans)
+	def __init__(self, input, states, trans, start=None):
+		self.sm = StateMachine(self, states, trans, start)
 		self.tape = [Cell(input[i]) for i in range(len(input))]
 		self.head = 0
 		
@@ -56,8 +56,11 @@ class Cell:
 		return self.char
 
 class StateMachine:
-	def __init__(self, tm, states, transitions):
-		self.curr = states[0]
+	def __init__(self, tm, states, transitions, start=None):
+		if start:
+			self.curr = start
+		else:
+			self.curr = states[0]
 		self.states = states
 		self.trans = transitions
 		self.tm = tm
@@ -79,7 +82,7 @@ class StateMachine:
 class State:
 	L = -1
 	R = 1
-	S = 0
+	# S = 0
 	def __init__(self, action, label=None):
 		self.action = action
 		self.label = label
@@ -117,21 +120,34 @@ class Transition:
 		return str((self.to, self.char, self.head))
 
 if __name__ == "__main__":
-	L = State.L; R = State.R; S = State.S
+	L = State.L; R = State.R
 
 	input = "$"+sys.argv[1]+"#"
 
 	states = [
-			('Head', None),
-			('Start', None),
-			('Accept', True),
-			('Reject', False)
+			('reject', False),
+			('1', None),
+			('2', None),
+			('3', None),
+			('4', None),
+			('5', None),
+			('6', None),
+			('7', None),
+			('8', None),
+			('9', None),
+			('accept', True),
 	]
 
 	transitions = {
-			(0, '$'): (1, None, R),
-			(1, '0'): (3, 'X', L),
-			(1, '1'): (2, None, L),
+		(1, '0'): (2,'$',R),	(1, '1'): (3,'$',R),	(1, '$'): (0,'#',R),	(1, 'X'): (0,'#',R),	(1, '#'): (10,'#',R),
+		(2, '0'): (2,'0',R),	(2, '1'): (3,'0',R),	(2, '$'): (0,'#',R),	(2, 'X'): (0,'#',R),	(2, '#'): (4,'0',L),
+		(3, '0'): (2,'1',R),	(3, '1'): (3,'1',R),	(3, '$'): (0,'#',R),	(3, 'X'): (0,'#',R),	(3, '#'): (4,'1',L),
+		(4, '0'): (4,'0',L),	(4, '1'): (4,'1',L),	(4, '$'): (5,'$',R),	(4, 'X'): (4,'X',L),	(4, '#'): (4,'#',L),
+		(5, '0'): (6,'0',R),	(5, '1'): (7,'X',L),	(5, '$'): (0,'#',R),	(5, 'X'): (5,'X',R),	(5, '#'): (10,'#',R),
+		(6, '0'): (6,'0',R),	(6, '1'): (7,'X',L),	(6, '$'): (0,'#',R),	(6, 'X'): (6,'X',R),	(6, '#'): (0,'#',R),
+		(7, '0'): (7,'0',L),	(7, '1'): (7,'1',L),	(7, '$'): (8,'$',R),	(7, 'X'): (7,'X',L),	(7, '#'): (7,'#',L),
+		(8, '0'): (9,'X',R),	(8, '1'): (8,'1',R),	(8, '$'): (0,'#',R),	(8, 'X'): (8,'X',R),	(8, '#'): (0,'#',R),
+		(9, '0'): (4,'X',L),	(9, '1'): (9,'1',R),	(9, '$'): (0,'#',R),	(9, 'X'): (9,'X',R),	(9, '#'): (0,'#',R),
 	}
 
 	states = [State(s[1], s[0]) for s in states]
@@ -141,5 +157,5 @@ if __name__ == "__main__":
 		(t, nc, h) = t
 		trans[(states[f], pc)] = Transition(states[t], nc, h)
 
-	machine = TuringMachine(input, states, trans)
+	machine = TuringMachine(input, states, trans, start=states[4])
 	machine.run()
